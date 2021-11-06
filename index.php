3a88061dee5ddbeffee26b075625eec2fc7d1d3a
Michael Chinye <michaelchinyechinye2018@gmail.com>
2021-11-06 03:17:17 -0700

    Removed admin files from PHP

--- index.php
+++ index.php
@@ -1,5 +1,1 @@
 <?php include('index.html'); ?>
-<?php include('adminIndex.html'); ?>
-<?php include('dashboard01.html'); ?>
-<?php include('dashboard02.html'); ?>
-<?php include('dashboard03.html'); ?>

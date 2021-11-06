55255a57449d5403cef0b62c25d0415f1146c89f
Michael Chinye <michaelchinyechinye2018@gmail.com>
2021-11-06 03:12:09 -0700

    included admin html files in PHP

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
 <?php include('index.html'); ?>
+<?php include('adminIndex.html'); ?>
+<?php include('dashboard01.html'); ?>
+<?php include('dashboard02.html'); ?>
+<?php include('dashboard03.html'); ?>
